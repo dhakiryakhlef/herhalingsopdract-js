e26e88f96d96770476cff47580a6ea5b652c146d
dhakiryakhlef <143789583+dhakiryakhlef@users.noreply.github.com>
2023-11-22 14:30:51 +0100

    wwew

--- json opdrachten/js.js
+++ json opdrachten/js.js
@@ -121,9 +121,74 @@ const array =`
     }
   ]`
 
+  const array1 = JSON.parse(array)
+for (let i = 0; i < array1.length; i++) {
+    const array2 = array1[i];
+    console.log(array2);
+}
 
 
 
+// Opdr. 6
+// -----------
+// Maak een string variabele met de volgende string waarde:
+
+const walahi =`
+[
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  },
+  {
+    "albumId": 1,
+    "id": 2,
+    "title": "reprehenderit est deserunt velit ipsam",
+    "url": "https://via.placeholder.com/600/771796",
+    "thumbnailUrl": "https://via.placeholder.com/150/771796"
+  },
+  {
+    "albumId": 1,
+    "id": 3,
+    "title": "officia porro iure quia iusto qui ipsa ut modi",
+    "url": "https://via.placeholder.com/600/24f355",
+    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+  },
+  {
+    "albumId": 1,
+    "id": 4,
+    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    "url": "https://via.placeholder.com/600/d32776",
+    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+  },
+  {
+    "albumId": 1,
+    "id": 5,
+    "title": "natus nisi omnis corporis facere molestiae rerum in",
+    "url": "https://via.placeholder.com/600/f66b97",
+    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+  },
+  {
+    "albumId": 1,
+    "id": 6,
+    "title": "accusamus ea aliquid et amet sequi nemo",
+    "url": "https://via.placeholder.com/600/56a8c2",
+    "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+  }
+]`
 
 
+const walahi2 = JSON.parse(walahi)
+
+for (let i = 0; i < walahi2.length; i++) {
+    const water = walahi2[i];
+    water = document.querySelector(".div1")
+    water.innerHTML
+}
+
+
+// Converteer dit json object naar JS object.
+// Toon op de webpagina de title en de url als <li> items in <ul>.
 
